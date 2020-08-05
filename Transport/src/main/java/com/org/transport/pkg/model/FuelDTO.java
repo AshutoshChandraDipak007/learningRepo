11234bc0dfb7beae1dedee7e69a7292656758971
AshutoshChandraDipak007 <54165959+AshutoshChandraDipak007@users.noreply.github.com>
2020-08-05 19:07:57 +0530

    Add files via upload
    commite

---
+++ Transport/src/main/java/com/org/transport/pkg/model/FuelDTO.java
@@ -0,0 +1,150 @@
+package com.org.transport.pkg.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Fuel_Entry")
+public class FuelDTO {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	private String plant;
+	private String vehicleNo;
+	private int openingKm;
+	private int closingKm;
+	private Float fuelLtr;
+	private int fuelAmmount;
+	private float efficiency;
+	private String make;
+	private String model;
+	private String driver;
+	private LocalDate date;
+	
+	public FuelDTO() {
+		super();
+		}
+
+	public FuelDTO(String plant, String vehicleNo, int openingKm, int closingKm, Float fuelLtr, int fuelAmmount,
+			float efficiency, String make, String model, String driver, LocalDate date) {
+		super();
+		this.plant = plant;
+		this.vehicleNo = vehicleNo;
+		this.openingKm = openingKm;
+		this.closingKm = closingKm;
+		this.fuelLtr = fuelLtr;
+		this.fuelAmmount = fuelAmmount;
+		this.efficiency = efficiency;
+		this.make = make;
+		this.model = model;
+		this.driver = driver;
+		this.date = date;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getPlant() {
+		return plant;
+	}
+
+	public void setPlant(String plant) {
+		this.plant = plant;
+	}
+
+	public String getVehicleNo() {
+		return vehicleNo;
+	}
+
+	public void setVehicleNo(String vehicleNo) {
+		this.vehicleNo = vehicleNo;
+	}
+
+	public int getOpeningKm() {
+		return openingKm;
+	}
+
+	public void setOpeningKm(int openingKm) {
+		this.openingKm = openingKm;
+	}
+
+	public int getClosingKm() {
+		return closingKm;
+	}
+
+	public void setClosingKm(int closingKm) {
+		this.closingKm = closingKm;
+	}
+
+	public Float getFuelLtr() {
+		return fuelLtr;
+	}
+
+	public void setFuelLtr(Float fuelLtr) {
+		this.fuelLtr = fuelLtr;
+	}
+
+	public int getFuelAmmount() {
+		return fuelAmmount;
+	}
+
+	public void setFuelAmmount(int fuelAmmount) {
+		this.fuelAmmount = fuelAmmount;
+	}
+
+	public float getEfficiency() {
+		return efficiency;
+	}
+
+	public void setEfficiency(float efficiency) {
+		this.efficiency = efficiency;
+	}
+
+	public String getMake() {
+		return make;
+	}
+
+	public void setMake(String make) {
+		this.make = make;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public String getDriver() {
+		return driver;
+	}
+
+	public void setDriver(String driver) {
+		this.driver = driver;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
+	
+
+	
+
+}
